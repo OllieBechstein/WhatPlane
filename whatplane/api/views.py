02524f4e0fb5a4d31ee9038e7f9f674c81d31652
OllieBechstein <olliebechstein@gmail.com>
2024-10-28 22:39:45 +0000

    Rewritten capturePlane to accept lat and lon, then capture the nearest plane

--- whatplane/api/views.py
+++ whatplane/api/views.py
@@ -5,6 +5,7 @@ from .serializers import PlaneSerializer
 from .models import Plane, UserProfile
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+import requests
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -56,8 +57,31 @@ def capturePlane(request):
     user_profile = request.user.userprofile
     
     data = request.data
-    type = data['type']
-    reg = data['reg']
+
+   
+    lat = data['lat']
+    lon = data['lon']
+
+    radius =50
+
+
+        # Make the external API call
+    response = requests.get(f"https://api.adsb.lol/v2/closest/{lat}/{lon}/{radius}")
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        adsb_data = response.json()  # Parse JSON response
+    else:
+        return Response({"error": "Failed to fetch data"}, status=response.status_code)
+
+    if not adsb_data["ac"]:
+        return Response({"detail": "No planes found"}, status=status.HTTP_200_OK)
+
+    plane_info = adsb_data["ac"][0]
+    type = plane_info.get("t")
+    reg = plane_info.get("r")
+
+    print(plane_info)
 
     if user_profile.planes.count() >=5:
         return Response({"detail": "You can only own 5 planes."}, status=status.HTTP_400_BAD_REQUEST)
